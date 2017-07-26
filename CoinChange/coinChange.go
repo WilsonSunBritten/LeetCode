4bb32ab4831e8393f83890e729e10762aed7404e
Wilson Britten <wilsonsunbritten@gmail.com>
2017-07-25 21:44:17 -0600

    Finished coinchanged, happy with dynamic solution that fully works now

--- CoinChange/coinChange.go
+++ CoinChange/coinChange.go
@@ -3,43 +3,61 @@ package main
 import "fmt"
 
 func main() {
-	testCoins := []int{1, 2, 5}
-	testAmount := 11
+	testCoins := []int{1, 4, 5}
+	testAmount := 12
 
 	result := coinChange(testCoins, testAmount)
 	fmt.Println("testCoins: ", testCoins, "\ntestAmount: ", testAmount, "\nresult", result)
 }
 
+type Node struct {
+	value int
+	count int
+}
+
 //result is an array of coin values that most efficiently add up to  amount
 func coinChange(coins []int, amount int) []int {
-	bestValues := make([]int, amount+1)
+	bestValues := make([]Node, amount+1)
 	//sort coins here, if necessary
 
 	//initialize starting coin values
 	for _, value := range coins {
 		if value < amount {
-			bestValues[value] = value
+			bestValues[value].value = value
+			bestValues[value].count = 1
 		}
 	}
-
+	fmt.Println("pre best values: ", bestValues, "\nend")
 	for index := 0; index <= amount; index++ {
-		for descIndex := len(coins) - 1; descIndex >= 0; descIndex-- {
-			if index-coins[descIndex] < 0 {
-				continue
-			} else {
-				if bestValues[index-coins[descIndex]] > 0 {
-					bestValues[index] = coins[descIndex]
-					break
+		bestCount := amount
+		bestValue := 0
+		for _, coinValue := range coins {
+			if coinValue == index {
+				bestCount = 0
+				bestValue = coinValue
+				break
+			}
+			if index-coinValue > 0 {
+				temp := bestValues[index-coinValue].count
+				if temp < bestCount {
+					bestValue = coinValue
+					bestCount = temp
 				}
 			}
-		}
-	}
 
+		}
+
+		bestValues[index].value = bestValue
+		bestValues[index].count = bestCount + 1
+	}
+	for index, value := range bestValues {
+		fmt.Println("index: ", index, "	value: ", value.value, "	count: ", value.count)
+	}
 	result := []int{}
 
 	for amount > 0 {
-		result = append(result, bestValues[amount])
-		amount -= bestValues[amount]
+		result = append(result, bestValues[amount].value)
+		amount -= bestValues[amount].value
 	}
 	return result
 }
